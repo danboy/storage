9af8094307677cbd902290c07b8c32bdaab78fa7
Dan Nawara <dan@pinmonkey.com>
2011-03-03 18:51:07 -0600

    sorage

--- javascript/sprout.js
+++ javascript/sprout.js
@@ -1,15 +1,15 @@
 var Sprout = function(canvas){
-  var soil = Raphael(canvas,200,400);
+  var soil = Raphael(canvas,600,1200);
   var stalk = soil.path(this.randomPath(5,9))
   stalk.attr({stroke: "#000000",'stroke-width': 2});
 }
 Sprout.prototype = {
 randomPath: function() {
-  var path = "M 50 0";
+  var path = "M 400 0";
   x = 10;
   y = 0;
   for (var i = 0; i < 10; i++) {
-    rand = Math.round(Math.random() * 200);
+    rand = Math.round(Math.random() * 20);
     if (i) {
       path += "C" + [x + 10, (y += 20) - 10, x, y, (x= 20 - rand), y];
     }
@@ -17,4 +17,3 @@ randomPath: function() {
   return path;
   }
 }
-//m 0,0 c 0,0 294.28571,62.85714 197.14285,288.57143
